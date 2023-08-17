192d99de9b1f061f8e448ba27fb2cf78bc27e995
Edwin Trigos <82897868+Ewin24@users.noreply.github.com>
2023-08-17 07:06:11 -0500

    Add files via upload

--- Ejercicios/retos/reto1.js
+++ Ejercicios/retos/reto1.js
@@ -2,22 +2,36 @@
 // ##                EJERCICIO 6                 ##
 // ################################################
 
-
 function newPassword(a, b) {
-    let arr1 = [], arr2 = [];
-    for (let i = 0; a.length < arr1.length; i++) {
+    let mergedString = '';
+
+    if (constraints(a, b)) {
+        if (a.length > b.length) {
+            while (condition) {
+                for (let i = 0; i < a.length; i++) {
+                    
+                }
+            }
+        }
 
     }
-    finalString = '';
-
-    return finalString;
+    return mergedString;
 }
 
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function constraints(a, b) {
+    flag = flase;
+    if (a.length >= 1 && b.length <= 25000) {
+        console.log("Longitud incorrecta en la palabra")
+    }
+    if ((/^([a-zA-ZñÑáéíóúÁÉÍÓÚ])+$/i.test(a)) && (/^([a-zA-ZñÑáéíóúÁÉÍÓÚ])+$/i.test(b))) {
+        console.log("Caracteres incorrectos")
+    } else {
+        console.log("Las entradas de datos son corerectas");
+        flag = true;
+    }
+    return flag;
 }
 
-console.log(getRandomInt(2));
-
-reto('abc', 'def')
+console.log(
+    newPassword('abc', 'def')
+);
